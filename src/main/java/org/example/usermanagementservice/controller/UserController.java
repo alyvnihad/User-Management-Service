e33e84f404d5b6c41b10c8946d4649cf072af0e8
alyvnihad <aliyevnihad011@gmail.com>
2025-11-17 14:40:33 +0400

    UserController class update.v1

--- src/main/java/org/example/usermanagementservice/controller/UserController.java
+++ src/main/java/org/example/usermanagementservice/controller/UserController.java
@@ -3,8 +3,10 @@ package org.example.usermanagementservice.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.usermanagementservice.dto.UserDTO;
 import org.example.usermanagementservice.payload.UserPayload;
 import org.example.usermanagementservice.service.UserService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,5 +23,31 @@ public class UserController {
         userService.register(payload);
         log.info("User create success");
         return ResponseEntity.ok("User create successfully");
+    }
+
+    @GetMapping("/getId/{id}")
+    public UserDTO getUserById(@PathVariable Long id) {
+        log.info("Fetched user with id {}", id);
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/all-users")
+    public Page<UserDTO> getAllUsers(@RequestParam(defaultValue = "0") int page,
+                                     @RequestParam(defaultValue = "10") int size) {
+        return userService.getAllUsers(page, size);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> userUpdate(@PathVariable Long id, @Valid @RequestBody UserPayload payload) {
+        userService.userUpdate(id, payload);
+        log.info("User update success");
+        return ResponseEntity.ok("User Update successfully");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> userDelete(@PathVariable Long id) {
+        userService.userDelete(id);
+        log.info("User deleting by id: {}", id);
+        return ResponseEntity.ok("User Deleting successfully");
     }
 }
